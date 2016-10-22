83d3183390c9491c4ba40ab52a7751d09d855fc9
dokgo <tomasromeo1998@gmail.com>
2016-10-23 01:56:44 +0300

    Animal deserialization

--- src/Main.java
+++ src/Main.java
@@ -13,91 +13,24 @@ import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        Client originalClient = new Client();
-        originalClient.setID(1);
-        originalClient.setName("Chuck Norris");
-        originalClient.setBirthDate(LocalDate.of(1940, 3, 10));
 
-        Path path = Paths.get("object.bin");
-        try (ObjectOutputStream oos = new ObjectOutputStream(Files.newOutputStream(path))) {
-            oos.writeObject(originalClient);
-        }
-
-        Client deserializedClient;
-        try (ObjectInputStream ois = new ObjectInputStream(Files.newInputStream(path))) {
-            deserializedClient = (Client) ois.readObject();
-        }
-
-        System.out.printf("%-15s %-30s\n", "ID", deserializedClient.getID());
-        System.out.printf("%-15s %-30s\n", "Name", deserializedClient.getName());
-        System.out.printf("%-15s %-30s\n", "Date of Birth", deserializedClient.getBirthDate());
-        System.out.printf("%-15s %-30s\n", "Age", deserializedClient.getAgeInYears());
 
     }
-    public static class Client implements Serializable {
-        private long id;
-        private String name;
-        private LocalDate birthDate;
-        private transient int ageInYears;
+    class Animal implements Serializable {
+        private final String name;
 
-        public long getID() {
-            return id;
-        }
-
-        public void setID(long id) {
-            this.id = id;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name) {
+        public Animal(String name) {
             this.name = name;
         }
 
-        public LocalDate getBirthDate() {
-            return birthDate;
-        }
-
-        public void setBirthDate(LocalDate birthDate) {
-            this.birthDate = birthDate;
-        }
-
-        public int getAgeInYears() {
-            if (ageInYears == 0) {
-                ageInYears = birthDate.until(LocalDate.now()).getYears();
+        @Override
+        public boolean equals(Object obj) {
+            if (obj instanceof Animal) {
+                return Objects.equals(name, ((Animal) obj).name);
             }
-            return ageInYears;
+            return false;
         }
     }
-
-
-
-    public static String readAsString(InputStream inputStream, Charset charset) throws IOException {
-
-        Reader reader = new InputStreamReader(inputStream, charset);
-        int ch;
-        String res = "";
-        while ((ch = reader.read()) != -1){
-            res += new String (Character.toChars(ch));
-        }
-        return res;
-
-        //System.out.println(Arrays.toString(arr));
-
-    }
-    public static void checkSumOfStream(InputStream inputStream) throws IOException {
-        int curr;
-        int tmp;
-        while((curr = inputStream.read()) != -1){
-            if(curr == 13 && (tmp = inputStream.read()) == 10){
-                curr = tmp;
-            }
-            System.out.println((byte)curr);
-        }
-    }
-
 
     public static List<Double> parseFile(String pathString) {
         Path path = Paths.get(pathString);
@@ -115,6 +48,21 @@ public class Main {
                 numbers.add(Double.parseDouble(s));
         }
         return numbers;
+    }
+    public static Animal[] deserializeAnimalArray(byte[] data) {
+        try(ObjectInputStream stream = new ObjectInputStream(new ByteArrayInputStream(data))){
+            int length = stream.readInt();
+            Animal[] animals = new Animal[length];
+            for (int i = 0; i < length; i++){
+                Object object = stream.readObject();
+                if(!(object instanceof Animal)) continue;
+                animals[i] = (Animal)object;
+            }
+            return animals;
+        }
+        catch (Exception e){
+            throw new IllegalArgumentException();
+        }
     }
 
     public static double mean(String path) {
