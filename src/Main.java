c642dcbc3229b78677807837b403cdeba7ee49ad
dokgo <tomasromeo1998@gmail.com>
2016-10-23 22:17:24 +0300

    symDiff

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
 
+import org.omg.PortableInterceptor.INACTIVE;
+
 import java.io.*;
 import java.nio.charset.Charset;
 import java.util.*;
@@ -13,9 +15,29 @@ import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) throws Exception {
+        HashSet<String> s1 = new HashSet<>();
+        s1.add("asd");
+        s1.add("qwe");
+        s1.add("zxc");
 
+        HashSet<String> s2 = new HashSet<>();
+        s2.add("asd");
+        s2.add("qw");
+        s2.add("zxc");
 
+        symmetricDifference(s1,s2);
+        System.out.println( symmetricDifference(s1,s2));
     }
+
+    public static <T> Set<T> symmetricDifference(Set<? extends T> set1, Set<? extends T> set2) {
+        HashSet<T> s1 = new HashSet<>(set1);
+        HashSet<T> s2 = new HashSet<>(set2);
+        s1.removeAll(set2);
+        s2.removeAll(set1);
+        s1.addAll(s2);
+        return s1;
+    }
+
     class Animal implements Serializable {
         private final String name;
 
@@ -48,6 +70,7 @@ public class Main {
                 numbers.add(Double.parseDouble(s));
         }
         return numbers;
+
     }
     public static Animal[] deserializeAnimalArray(byte[] data) {
         try(ObjectInputStream stream = new ObjectInputStream(new ByteArrayInputStream(data))){
